c4731d561c9e9655f8dbda9d30cd38b09d5fb0ee
Solpooh <hsw9420@naver.com>
2024-06-11 18:31:30 +0900

    [feat] Response Code & Message (Back)

---
+++ board-back/src/main/java/com/solpooh/boardback/dto/response/ResponseDto.java
@@ -0,0 +1,22 @@
+package com.solpooh.boardback.dto.response;
+
+import com.solpooh.boardback.common.ResponseCode;
+import com.solpooh.boardback.common.ResponseMessage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+    private String code;
+    private String message;
+
+    // 공용으로 DB에러를 가지고 있음
+    public static ResponseEntity<ResponseDto> databaseError() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+}
