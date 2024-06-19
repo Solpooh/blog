24cfedf4dd813bc79e2fa05348ba6658c3db5c84
Solpooh <hsw9420@naver.com>
2024-06-19 12:17:56 +0900

    [feat] Header 레이아웃 작성 (로그인,로그아웃)

--- board-front/src/types/interface/index.ts
+++ board-front/src/types/interface/index.ts
@@ -1,7 +1,9 @@
+import User from './user.interface';
 import BoardListItem from './board-list-item.interface';
 import CommentListItem from './comment-list-item.interface';
 import FavoriteListItem from './favorite-list-item.interface';
 export type {
+    User,
     BoardListItem,
     CommentListItem,
     FavoriteListItem
