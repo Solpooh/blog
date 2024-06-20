609f8a31501203fe2622ca9ed0930b37cb007202
Solpooh <hsw9420@naver.com>
2024-06-20 13:37:43 +0900

    [feat] Entity, Repository 작성

---
+++ board-back/src/main/java/com/solpooh/boardback/repository/BoardListViewRepository.java
@@ -0,0 +1,10 @@
+package com.solpooh.boardback.repository;
+
+import com.solpooh.boardback.entity.BoardListViewEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardListViewRepository extends JpaRepository<BoardListViewEntity, Integer> {
+
+}
---
+++ board-back/src/main/java/com/solpooh/boardback/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.solpooh.boardback.repository;
+
+import com.solpooh.boardback.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+}
