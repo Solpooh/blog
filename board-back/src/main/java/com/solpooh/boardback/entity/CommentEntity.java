99dc970fa43873f21da61ba731dde4d31b729b8a
Solpooh <hsw9420@naver.com>
2024-07-01 12:03:02 +0900

    [feat] 게시물 댓글 작성 API 구현

--- board-back/src/main/java/com/solpooh/boardback/entity/CommentEntity.java
+++ board-back/src/main/java/com/solpooh/boardback/entity/CommentEntity.java
@@ -1,9 +1,14 @@
 package com.solpooh.boardback.entity;
 
+import com.solpooh.boardback.dto.request.board.PostCommentRequestDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 
 @Getter
 @NoArgsConstructor
@@ -17,4 +22,15 @@ public class CommentEntity {
     private String writeDatetime;
     private String userEmail;
     private int boardNumber;
+
+    public CommentEntity(PostCommentRequestDto dto, Integer boardNumber, String email) {
+        Date now = Date.from(Instant.now());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String writeDatetime = simpleDateFormat.format(now);
+
+        this.content = dto.getContent();
+        this.writeDatetime = writeDatetime;
+        this.userEmail = email;
+        this.boardNumber = boardNumber;
+    }
 }
