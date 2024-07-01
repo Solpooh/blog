be7ed54255405f119882353905891460d2505453
Solpooh <hsw9420@naver.com>
2024-07-01 10:41:41 +0900

    [feat] 좋아요 리스트 불러오기 API 구현

--- board-back/src/main/java/com/solpooh/boardback/service/BoardService.java
+++ board-back/src/main/java/com/solpooh/boardback/service/BoardService.java
@@ -2,12 +2,14 @@ package com.solpooh.boardback.service;
 
 import com.solpooh.boardback.dto.request.board.PostBoardRequestDto;
 import com.solpooh.boardback.dto.response.board.GetBoardResponseDto;
+import com.solpooh.boardback.dto.response.board.GetFavoriteListResponseDto;
 import com.solpooh.boardback.dto.response.board.PostBoardResponseDto;
 import com.solpooh.boardback.dto.response.board.PutFavoriteResponseDto;
 import org.springframework.http.ResponseEntity;
 
 public interface BoardService {
     ResponseEntity<? super GetBoardResponseDto> getBoard(Integer boardNumber);
+    ResponseEntity<? super GetFavoriteListResponseDto> getFavoriteList(Integer boardNumber);
     ResponseEntity<? super PostBoardResponseDto> postBoard(PostBoardRequestDto dto, String email);
     ResponseEntity<? super PutFavoriteResponseDto> putFavorite(Integer boardNumber, String email);
 }
