31945ceefdf16b933fe773272e5e5b9810e28222
Solpooh <hsw9420@naver.com>
2024-07-03 17:25:55 +0900

    [feat] pagination 컴포넌트 작성

--- board-front/src/hooks/pagination.hook.ts
+++ board-front/src/hooks/pagination.hook.ts
@@ -1,21 +1,21 @@
 import {useEffect, useState} from 'react';
 
 const usePagination = <T> (countPerPage: number) => {
-    //  state: 전체 객체 리스트 상태 //
+    //  state: 전체 객체 리스트 상태 (총 게시물) //
     const [totalList, setTotalList] = useState<T[]>([]);
-    //  state: 보여줄 객체 리스트 상태 //
+    //  state: 보여줄 객체 리스트 상태 (한 페이지에 보여줄 게시물 수) //
     const [viewList, setViewList] = useState<T[]>([]);
-    //  state: 현재 페이지 번호 상태 //
+    //  state: 현재 페이지 번호 상태 (page) //
     const [currentPage, setCurrentPage] = useState<number>(1);
 
-    //  state: 전체 페이지 번호 리스트 상태 //
+    //  state: 전체 페이지 번호 리스트 상태 [1, 2, ... 35] //
     const [totalPageList, setTotalPageList] = useState<number[]>([1]);
-    //  state: 보여줄 페이지 번호 리스트 상태 //
+    //  state: 보여줄 페이지 번호 리스트 상태 [1, 2, 3, ... 10] //
     const [viewPageList, setViewPageList] = useState<number[]>([1]);
-    //  state: 현재 섹션 상태 //
+    //  state: 현재 섹션 상태 [11, 12, 13 ... 20] == 2 //
     const [currentSection, setCurrentSection] = useState<number>(1);
 
-    //  state: 마지막 섹션 상태 //
+    //  state: 마지막 섹션 상태 [31, 32 ... 35] //
     const [totalSection, setTotalSection] = useState<number>(1);
 
     //  function: 보여줄 객체 리스트 추출 함수 //
@@ -27,7 +27,7 @@ const usePagination = <T> (countPerPage: number) => {
     };
     //  function: 보여줄 페이지 리스트 추출 함수 //
     const setViewPage = () => {
-        const FIRST_INDEX = 10 * (currentSection -1);
+        const FIRST_INDEX = 10 * (currentSection - 1);
         const LAST_INDEX = totalPageList.length > 10 * currentSection ? 10 * currentSection : totalPageList.length;
         const viewPageList = totalPageList.slice(FIRST_INDEX, LAST_INDEX);
         setViewPageList(viewPageList);
