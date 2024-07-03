75eadb36bdce4a3d02cccf9eec1f490ad9afa9cd
Solpooh <hsw9420@naver.com>
2024-07-03 11:28:06 +0900

    [feat] 게시물 상세 댓글 작성 API 구현

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -3,13 +3,13 @@ import axios from "axios";
 import {SignInResponseDto, SignUpResponseDto} from './response/auth';
 import {ResponseDto} from './response';
 import {GetSignInUserResponseDto} from './response/user';
-import {PostBoardRequestDto} from './request/board';
+import {PostBoardRequestDto, PostCommentRequestDto} from './request/board';
 import {
     PostBoardResponseDto,
     GetBoardResponseDto,
     IncreaseViewCountResponseDto,
     GetFavoriteListResponseDto,
-    GetCommentListResponseDto, PutFavoriteResponseDto
+    GetCommentListResponseDto, PutFavoriteResponseDto, PostCommentResponseDto
 } from './response/board';
 
 const DOMAIN = 'http://localhost:4000';
@@ -55,6 +55,7 @@ const INCREASE_VIEW_COUNT_URL = (boardNumber: number | string) => `${API_DOMAIN}
 const GET_FAVORITE_LIST_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/favorite-list`;
 const GET_COMMENT_LIST_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/comment-list`;
 const POST_BOARD_URL = () => `${API_DOMAIN}/board`;
+const POST_COMMENT_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/comment`;
 const PUT_FAVORITE_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/favorite`;
 
 export const getBoardRequest = async (boardNumber: number | string) => {
@@ -113,6 +114,19 @@ export const postBoardRequest = async (requestBody: PostBoardRequestDto, accessT
     const result = await axios.post(POST_BOARD_URL(), requestBody, authorization(accessToken))
         .then(response => {
             const responseBody: PostBoardResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            if (!error.response) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+        });
+    return result;
+}
+export const postCommentRequest = async (boardNumber: number | string, requestBody: PostCommentRequestDto, accessToken: string) => {
+    const result = await axios.post(POST_COMMENT_URL(boardNumber), requestBody, authorization(accessToken))
+        .then(response => {
+            const responseBody: PostCommentResponseDto = response.data;
             return responseBody;
         })
         .catch(error => {
--- board-front/src/apis/response/board/index.ts
+++ board-front/src/apis/response/board/index.ts
@@ -1,5 +1,6 @@
 import GetBoardResponseDto from './get-board.response.dto';
 import PostBoardResponseDto from './post-board.response.dto';
+import PostCommentResponseDto from './post-comment.response.dto';
 import PutFavoriteResponseDto from './put-favorite.response.dto';
 import GetCommentListResponseDto from './get-comment-list.response.dto';
 import GetFavoriteListResponseDto from './get-favorite-list.response.dto';
@@ -8,6 +9,7 @@ import IncreaseViewCountResponseDto from './increase-view-cout.response.dto';
 export type {
     GetBoardResponseDto,
     PostBoardResponseDto,
+    PostCommentResponseDto,
     PutFavoriteResponseDto,
     GetCommentListResponseDto,
     GetFavoriteListResponseDto,
