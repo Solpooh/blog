23b037265835ce374f88cff6346b2c6b5462c9b1
Solpooh <hsw9420@naver.com>
2024-07-05 16:38:38 +0900

    [feat] 검색 게시물 리스트 불러오기 API 작성

--- board-back/src/main/java/com/solpooh/boardback/controller/BoardController.java
+++ board-back/src/main/java/com/solpooh/boardback/controller/BoardController.java
@@ -54,6 +54,14 @@ public class BoardController {
         ResponseEntity<? super GetTop3BoardListResponseDto> response = boardService.getTop3BoardList();
         return response;
     }
+    @GetMapping(value = {"/search-list/{searchWord}", "/search-list/{searchWord}/{preSearchWord}"})
+    public ResponseEntity<? super GetSearchBoardListResponseDto> getSearchBoardList(
+            @PathVariable("searchWord") String searchWord,
+            @PathVariable(value = "preSearchWord", required = false) String preSearchWord
+    ) {
+        ResponseEntity<? super GetSearchBoardListResponseDto> response = boardService.getSearchBoardList(searchWord, preSearchWord);
+        return response;
+    }
     @PostMapping("")
     public ResponseEntity<? super PostBoardResponseDto> postBoard(
             @RequestBody @Valid PostBoardRequestDto requestBody,
