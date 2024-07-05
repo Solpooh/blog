7b24f651afae0dce7b36072d423e1ef2066825f6
Solpooh <hsw9420@naver.com>
2024-07-05 10:46:37 +0900

    [feat] 인기 검색어 불러오기 API 작성

--- board-back/src/main/java/com/solpooh/boardback/repository/SearchLogRepository.java
+++ board-back/src/main/java/com/solpooh/boardback/repository/SearchLogRepository.java
@@ -1,10 +1,23 @@
 package com.solpooh.boardback.repository;
 
 import com.solpooh.boardback.entity.SearchLogEntity;
+import com.solpooh.boardback.repository.resultSet.GetPopularListResultSet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SearchLogRepository extends JpaRepository<SearchLogEntity, Integer> {
-
+    @Query(value =
+    "SELECT search_word as searchWord, count(search_word) AS count " +
+    "FROM search_log " +
+    "WHERE relation IS FALSE " +
+    "GROUP BY search_word " +
+    "ORDER BY count DESC " +
+    "LIMIT 15 ",
+    nativeQuery = true
+    )
+    List<GetPopularListResultSet> getPopularList();
 }
