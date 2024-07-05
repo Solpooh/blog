4f6c1bde542704434bdaa65dad6734882dba1751
Solpooh <hsw9420@naver.com>
2024-07-05 16:38:38 +0900

    [feat] 검색 게시물 리스트 불러오기 API 작성

---
+++ board-back/src/main/java/com/solpooh/boardback/dto/response/board/GetSearchBoardListResponseDto.java
@@ -0,0 +1,27 @@
+package com.solpooh.boardback.dto.response.board;
+
+import com.solpooh.boardback.common.ResponseCode;
+import com.solpooh.boardback.common.ResponseMessage;
+import com.solpooh.boardback.dto.object.BoardListItem;
+import com.solpooh.boardback.dto.response.ResponseDto;
+import com.solpooh.boardback.entity.BoardListViewEntity;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Getter
+public class GetSearchBoardListResponseDto extends ResponseDto {
+    private List<BoardListItem> searchList;
+
+    private GetSearchBoardListResponseDto(List<BoardListViewEntity> boardListViewEntities) {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.searchList = BoardListItem.getList(boardListViewEntities);
+    }
+
+    public static ResponseEntity<GetSearchBoardListResponseDto> success(List<BoardListViewEntity> boardListViewEntities) {
+        GetSearchBoardListResponseDto result = new GetSearchBoardListResponseDto(boardListViewEntities);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
--- board-back/src/main/java/com/solpooh/boardback/service/implement/BoardServiceImplement.java
+++ board-back/src/main/java/com/solpooh/boardback/service/implement/BoardServiceImplement.java
@@ -30,6 +30,7 @@ public class BoardServiceImplement implements BoardService {
     private final ImageRepository imageRepository;
     private final CommentRepository commentRepository;
     private final FavoriteRepository favoriteRepository;
+    private final SearchLogRepository searchLogRepository;
     private final BoardListViewRepository boardListViewRepository;
 
     @Override
@@ -117,6 +118,31 @@ public class BoardServiceImplement implements BoardService {
             return ResponseDto.databaseError();
         }
         return GetTop3BoardListResponseDto.success(boardListViewEntities);
+    }
+
+    @Override
+    public ResponseEntity<? super GetSearchBoardListResponseDto> getSearchBoardList(String searchWord, String preSearchWord) {
+        List<BoardListViewEntity> boardListViewEntities = new ArrayList<>();
+
+        try {
+
+            boardListViewEntities = boardListViewRepository.findByTitleContainsOrContentContainsOrderByWriteDatetimeDesc(searchWord, searchWord);
+
+            SearchLogEntity searchLogEntity = new SearchLogEntity(searchWord, preSearchWord, false);
+            searchLogRepository.save(searchLogEntity);
+
+            boolean relation = preSearchWord != null;
+            if (relation) {
+                searchLogEntity = new SearchLogEntity(preSearchWord, searchWord, true);
+                searchLogRepository.save(searchLogEntity);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetSearchBoardListResponseDto.success(boardListViewEntities);
     }
 
     @Override
