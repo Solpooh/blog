c993bbddea854d2f70d6f2d049c8185293dffc8e
Solpooh <hsw9420@naver.com>
2024-07-07 17:29:39 +0900

    [feat] 유저 프로필 정보 불러오기 API 작성

--- board-back/src/main/java/com/solpooh/boardback/service/UserService.java
+++ board-back/src/main/java/com/solpooh/boardback/service/UserService.java
@@ -1,8 +1,10 @@
 package com.solpooh.boardback.service;
 
 import com.solpooh.boardback.dto.response.user.GetSignInUserResponseDto;
+import com.solpooh.boardback.dto.response.user.GetUserResponseDto;
 import org.springframework.http.ResponseEntity;
 
 public interface UserService {
+    ResponseEntity<? super GetUserResponseDto> getUser(String email);
     ResponseEntity<? super GetSignInUserResponseDto> getSignInUser(String email);
 }
