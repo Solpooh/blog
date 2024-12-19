f78efc1eda25e42e7ec65d2680bde113bdf860c6
Solpooh <hsw9420@naver.com>
2024-12-19 14:47:59 +0900

    [feat] AWS S3 업로드 구현

---
+++ board-back/src/main/java/com/solpooh/boardback/config/S3Config/ProdS3Config.java
@@ -0,0 +1,18 @@
+package com.solpooh.boardback.config.S3Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+
+@Configuration
+@Profile("prod")
+public class ProdS3Config {
+    @Bean
+    public S3AsyncClient prodS3AsyncClient() {
+        return S3AsyncClient.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .build();
+    }
+}
